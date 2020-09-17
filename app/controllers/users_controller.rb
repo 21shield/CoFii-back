4bde762fa7df9149ce167dc72b4b392ce353d98c
21shield <21netaly@gmail.com>
2020-09-17 12:48:01 -0400

    refactored user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,29 +2,42 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-            render json:{ user: UserSerializer.new(user)}, status: :created
+            token = JWT.encode({ user_id: user.id}, "so_secret", 'HS256')
+
+            render json:{ user: UserSerializer.new(user), token: token}, status: :created
 
         else
-            render json: { error: 'failed to create user'}, status: :not_acceptable
+            render json: { error: user.errors.full_messages}, status: :not_acceptable
             
         end
     end
 
     def login
         user = User.find_by(username: user_params[:username])
+
         if user && user.authenticate(params[:password])
-            render json: user
+
+            token = JWT.encode({ user_id: user.id}, "so_secret", 'HS256')
+            render json: {user: UserSerializer.new(user), token: token} 
         else
+
             render json: { message: 'INVALID USERNAME OR PASSWORD'}, status: :unauthorized
         end
     end
+    
 
     def autologin
+        auth_header = request.headers['Authorization']
+        token = auth_header.split(' ')[1]
+
+        decoded_token = JWT.decode(token, 'so_secret', true, {algorithm:'HS256'})
+        userId = decoded_token[0]['user_id']
         user = User.find_by()
+
         if user
             render json: {user: UserSerializer.new(user)}, status: :authorized
         else
-            render json: {error: "INVALID LOGIN"}, status: :unauthorized
+            render json: {error: user.errors.full_messages}, status: :unauthorized
         end
 
     end
