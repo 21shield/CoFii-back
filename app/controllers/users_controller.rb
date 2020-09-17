6357bf51da0e24d79295e2b92049d1d7f9f3ef84
21shield <21netaly@gmail.com>
2020-09-17 09:19:32 -0400

    added user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password, :email, :bio, :avatar)
+        params.require(:user).permit(:username, :password,:password_confirmation, :email, :bio, :avatar)
     end
 
 
