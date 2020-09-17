015ef0cb1b72a2444e8a98991f000a7ba2c9f2b6
21shield <21netaly@gmail.com>
2020-09-16 20:19:04 -0400

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json:{user: UserSerializer.new(users), status: :created}
+
+        else
+            render json: {error: 'failed to create user'}, status: :not_acceptable
+            
+        end
+    end
+
+    # private methods
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email, :bio, :avatar)
+    end
+
+
+end
