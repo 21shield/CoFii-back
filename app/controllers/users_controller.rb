a7d39e0d38cc8f145a020d62858e148a448a01ce
21shield <21netaly@gmail.com>
2020-09-17 11:55:31 -0400

    added logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,31 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-            render json:{user: UserSerializer.new(users), status: :created}
+            render json:{ user: UserSerializer.new(user)}, status: :created
 
         else
-            render json: {error: 'failed to create user'}, status: :not_acceptable
+            render json: { error: 'failed to create user'}, status: :not_acceptable
             
         end
+    end
+
+    def login
+        user = User.find_by(username: user_params[:username])
+        if user && user.authenticate(params[:password])
+            render json: user
+        else
+            render json: { message: 'INVALID USERNAME OR PASSWORD'}, status: :unauthorized
+        end
+    end
+
+    def autologin
+        user = User.find_by()
+        if user
+            render json: {user: UserSerializer.new(user)}, status: :authorized
+        else
+            render json: {error: "INVALID LOGIN"}, status: :unauthorized
+        end
+
     end
 
     # private methods
@@ -15,7 +34,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password,:password_confirmation, :email, :bio, :avatar)
+        params.permit(:username, :password,:password_confirmation, :email, :bio, :avatar)
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users
+  post '/signup', to: 'users#create'
+  post '/login', to: 'users#login'
+  get '/autologin', to: 'users#autologin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
